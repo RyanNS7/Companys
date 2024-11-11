1c5289924517c7f6121233605bb60be73e92407d
Ryanns7 <ryanns777@gmail.com>
2024-11-11 11:45:00 -0300

    index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -85,11 +85,10 @@ app.get("/task", async (req: Request, res: Response) => {
     await prisma.$connect()
 
     try {
-        const id_servicePosition = await prisma.companyPosition.findFirst({where: {companyCNPJ: parseInt(req.body.company_cnpj), servicePosition: req.body.position}})
 
-        const employee = await prisma.employee.create({data: {name: req.body.name, companyCNPJ: parseInt(req.body.company_cnpj), position: id_servicePosition.id}})
+        const task = await prisma.task.create({data: {title: req.body.title, priority: true, completed: false, description: req.body.description}})
 
-        res.send(new EmployeeDTO({...employee, position: id_servicePosition.servicePosition}))
+        res.send(task)
 
     } catch (error) {
         res.send(error)
@@ -101,11 +100,23 @@ app.get("/group", async (req: Request, res: Response) => {
     await prisma.$connect()
 
     try {
-        const id_servicePosition = await prisma.companyPosition.findFirst({where: {companyCNPJ: parseInt(req.body.company_cnpj), servicePosition: req.body.position}})
+        const group = await prisma.group.create({data: {task: req.body.task}})
 
-        const employee = await prisma.employee.create({data: {name: req.body.name, companyCNPJ: parseInt(req.body.company_cnpj), position: id_servicePosition.id}})
+        res.send(group)
 
-        res.send(new EmployeeDTO({...employee, position: id_servicePosition.servicePosition}))
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+app.get("/employeegroup", async (req: Request, res: Response) => {
+
+    await prisma.$connect()
+
+    try {
+        const group = await prisma
+
+        res.send(group)
 
     } catch (error) {
         res.send(error)
