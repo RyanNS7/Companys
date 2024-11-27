3fed98728d7ceea7e60db93e7acf58b88b1036e6
Ryanns7 <ryanns777@gmail.com>
2024-11-26 22:09:12 -0300

    refactor: refactoring group creation controller code

--- backend/src/adapters/controllers/group/createGroupController.ts
+++ backend/src/adapters/controllers/group/createGroupController.ts
@@ -1,38 +1,33 @@
 import { createGroupUseCase } from "../../../application/usecases/group/createGroupUseCase"
 import { BadRequestError } from "../../../domain/errors/BadRequestError"
 import { NotFoundError } from "../../../domain/errors/NotFoundError"
-import { EmployeeRepo } from "../../../domain/usecases/employeeRepo"
 import { GroupRepo } from "../../../domain/usecases/groupRepo"
+import { TaskRepo } from "../../../domain/usecases/taskRepo"
 import { httpRequest, httpResponse } from "../../../http/http"
 import { NotFound, ServerError, badRequest, created } from "../../../http/statusCode"
 
 export class createGroupController{
-    employeeRepo: EmployeeRepo
+    taskRepo: TaskRepo
     groupRepo: GroupRepo
 
-    constructor(employeeRepo: EmployeeRepo, groupRepo: GroupRepo){
-        this.employeeRepo = employeeRepo
+    constructor(taskRepo: TaskRepo, groupRepo: GroupRepo){
+        this.taskRepo = taskRepo
         this.groupRepo = groupRepo
     }
 
     async create(httpRequest: httpRequest): Promise<httpResponse>{
 
         const request = {
-            id_manager: (httpRequest.body as {id_manager: string}).id_manager,
-            id_employees: (httpRequest.body as {id_employees: string[]}).id_employees
+            id_task: (httpRequest.body as {id_task: string}).id_task,
         }
 
-        if(request.id_manager === undefined){
-            return badRequest("Error, id manager is required")
-        }
-
-        if(request.id_employees === undefined){
-            return badRequest("Error, id employees is required")
+        if(request.id_task === undefined){
+            return badRequest("Error, id task is required")
         }
 
         try {
 
-            const createGroup = await new createGroupUseCase(this.employeeRepo, this.groupRepo).create(request.id_manager, request.id_employees)
+            const createGroup = await new createGroupUseCase(this.taskRepo, this.groupRepo).create(request.id_task)
 
             if(createGroup instanceof BadRequestError){
                 return badRequest(createGroup.message)
