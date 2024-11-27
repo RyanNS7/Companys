67a23fc4b1801c9bdab6931d3218e3d672d16283
Ryanns7 <ryanns777@gmail.com>
2024-11-26 22:27:03 -0300

    refactor: changing the typing of the function parameter

--- backend/src/infra/DB/task/createTaskInDB.ts
+++ backend/src/infra/DB/task/createTaskInDB.ts
@@ -1,13 +1,13 @@
-import { Task } from "@prisma/client";
 import { BadRequestError } from "../../../domain/errors/BadRequestError";
 import { prisma } from "../prisma";
 import { TaskDTO } from "../../../domain/entities/task/taskDTO";
+import { Task } from "../../../domain/entities/task/task";
 
 export async function createTask(task: Task): Promise<TaskDTO | BadRequestError>{
 
     try {
 
-        const taskDB = await prisma.task.create({data: {title: task.title, completed: false, priority: task.priority, description: task.description}})
+        const taskDB = await prisma.task.create({data: {title: task.task.title, completed: false, priority: task.task.priority, description: task.task.description}})
         
         return new TaskDTO(taskDB)
 
