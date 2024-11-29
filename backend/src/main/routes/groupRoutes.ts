850b0b9ed58a71b80f916adbd470a32b9ec8964c
Ryanns7 <ryanns777@gmail.com>
2024-11-29 12:20:28 -0300

    feat: group routes

---
+++ backend/src/main/routes/groupRoutes.ts
@@ -0,0 +1,22 @@
+import { addAnotherEmployeeToTheGroupController } from "../../adapters/controllers/group/addAnotherEmployeeToTheGroupController";
+import { createGroupController } from "../../adapters/controllers/group/createGroupController";
+import { EmployeeRepository } from "../../infra/implementations/employeeRepository";
+import { GroupRepository } from "../../infra/implementations/groupRepository";
+import { taskRepository } from "../../infra/implementations/taskRepository";
+import Express, { Request, Response } from "express";
+
+const groupRouter = Express.Router()
+
+groupRouter.post("/create", async (req: Request, res: Response) => {
+    const group = await new createGroupController(new taskRepository, new GroupRepository).create(req)
+
+    res.json(group.body).status(group.statusCode)
+})
+
+groupRouter.post("/newEmployee", async (req: Request, res: Response) => {
+    const addNewEmployee = await new addAnotherEmployeeToTheGroupController(new EmployeeRepository, new GroupRepository).addEmployee(req)
+
+    res.json(addNewEmployee.body).status(addNewEmployee.statusCode)
+})
+
+export { groupRouter }
