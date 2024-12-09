930ee6bdb5b572c7486334fd02cc88947bad0912
Ryanns7 <ryanns777@gmail.com>
2024-12-08 22:40:40 -0300

    feat: verification of the company's position before creating the employee

--- backend/src/infra/DB/employee/createEmployeeInDB.ts
+++ backend/src/infra/DB/employee/createEmployeeInDB.ts
@@ -1,17 +1,24 @@
-import { EmployeeDTO} from "../../../domain/entities/employee/employeDTO";
+import { EmployeeDTO} from "../../../domain/entities/employee/employeeDTO";
 import { IEmployee } from "../../../domain/entities/employee/employee";
 import { BadRequestError } from "../../../domain/errors/BadRequestError";
+import { NotFoundError } from "../../../domain/errors/NotFoundError";
 import { prisma } from "../prisma";
 
-export async function createEmployee(employee: IEmployee): Promise<EmployeeDTO | BadRequestError> {
+export async function createEmployee(employee: IEmployee): Promise<EmployeeDTO | BadRequestError | NotFoundError> {
     
     try {
 
-        const id_servicePosition = await prisma.companyPosition.findFirst({where: {companyCNPJ: parseInt(employee.company_CNPJ), servicePosition: employee.company_position}})
-        
-        const employeeDB = await prisma.employee.create({data: {name: employee.name, position: id_servicePosition.id, companyCNPJ: parseInt(employee.company_CNPJ)}})
+        try {
 
-        return new EmployeeDTO({...employeeDB, position: employee.company_position})
+            const id_servicePosition = await prisma.companyPosition.findFirstOrThrow({where: {companyCNPJ: BigInt(employee.company_CNPJ), servicePosition: employee.company_position}})
+            
+            const employeeDB = await prisma.employee.create({data: {name: employee.name, position: id_servicePosition.id, companyCNPJ: BigInt(employee.company_CNPJ)}})
+
+            return new EmployeeDTO({...employeeDB,companyCNPJ: employeeDB.companyCNPJ.toString(), position: employee.company_position})
+        } catch (error) {
+            return new NotFoundError("Service Position Not Found")
+        }
+
     } catch (error) {
         return new BadRequestError(error.name)
     }
